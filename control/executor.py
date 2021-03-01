dbfaf1b21ac5d2f52ac5567136864dbd5ae2f07b
Mikhail Parkun <mikhail.parkun@epicgames.com>
2021-03-01 10:14:32 +0300

    Handle CSV

--- control/executor.py
+++ control/executor.py
@@ -4,29 +4,30 @@ import logging
 logger = logging.getLogger("main")
 
 
-class Executor(object):
+class ScriptExecutor(object):
 
     def __init__(self):
         self.exit_code = None
-        self.full_output = None
-        self.stripped_output = None
         self.error_output = None
 
-    def execute(self, command, from_dir=None):
+    def execute(self, script):
         """Call bash command
 
         return bash output or just a command if no output
 
         """
         try:
-            output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, cwd=from_dir)
+            # put additional debug logging here
+            # put output assignment to script
+            logger.info(f'Executing {script.command_with_arg} (dir = {script.exec_dir})')
+            output = subprocess.check_output(script.command_with_arg, shell=True, stderr=subprocess.STDOUT, cwd=script.exec_dir)
         except OSError as os_exc:
-            logger.error("FAILED to run bash command (OSError):\n{0}".format(command))
+            logger.error("FAILED to run bash command (OSError):\n{0}".format(script.command))
             logger.error(os_exc)
             exit(1)
             return False
         except subprocess.CalledProcessError as exc:
-            logger.warning("FAILED to run bash command (CalledProcessError):\n{0}".format(command))
+            logger.warning("FAILED to run bash command (CalledProcessError):\n{0}".format(script.command))
             logger.warning("===CalledProcessError================================:")
             logger.warning("Return code: {0}".format(exc.returncode))
             logger.warning("Command:\n{0}".format(exc.cmd))
@@ -37,8 +38,8 @@ class Executor(object):
             return False
         else:
             self.exit_code = 0
-            self.full_output = output
-            self.stripped_output = self._strip_output(output)
+            script.output_full = output
+            script.output_strip = self._strip_output(output)
             return True
 
     def _strip_output(self, bin_string_output):
--- start.py
+++ start.py
@@ -1,7 +1,11 @@
+import csv
+import json
 import logging
 import os
 
-from control.executor import Executor
+import deepdiff  # pip3 install deepdiff
+
+from control.executor import ScriptExecutor
 
 """Execute bash script and analyse results
 
@@ -12,10 +16,12 @@ Check bash script results
     - check_substring_like
     - highlight_expected_with_student
     TODO
-    - Working with students dirs/zip ???
+    - Preparations => Iterate through students dirs/zip ???
     - Generate full report to file + score
     - YAML config
     - Wide test set => config.yaml
+    - test cases generator ???
+    - true script comparator
 
 """
 
@@ -35,59 +41,119 @@ logger.addHandler(handler_console)
 logger.debug("Running {0}".format(__file__))
 
 
-class Script(object):
+class ResultCheckerCSV(object):
+    def __init__(self, expected_csv, to_check_csv):
+        self.expected_result_csv = expected_csv
+        self.to_check_csv = to_check_csv
+        self.content_expected, self.expected_fields = self._get_csv_content(self.expected_result_csv)
+        self.content_to_check, self.to_check_fields = self._get_csv_content(self.to_check_csv)
 
-    def __init__(self, command, testing_arg, result_expected):
-        # TODO work with bunch of args set -> results set
+    def _get_csv_content(self, csv_file_path):
+        """Get list of CSV content
+        return list [{row1}, {row2}, ...]
+
+        """
+
+        logger.info(f"Getting CSV content {csv_file_path}")
+        output_list = []
+
+        with open(csv_file_path, newline='') as csvfile:
+
+            reader = csv.DictReader(csvfile)
+            try:
+                for row in reader:
+                    output_list.append(row)
+            except csv.Error as e:
+                print("csv Reader error")
+                print(e)
+            else:
+                return output_list, reader.fieldnames
+
+    def compare_dictionaries(self):
+        length1 = len(self.content_expected)
+        length2 = len(self.content_expected)
+        if self.expected_fields != self.to_check_fields:
+            logger.info(f"{self.to_check_fields} != {self.expected_fields}")
+            logger.info("Not matched fields")
+            return False
+        for i in range(length1):
+            logger.debug("Comparing CSV content:")
+            logger.debug(f'\t{self.content_expected[i]} \n\t<=> {self.content_to_check[i]}')
+            result = self.content_expected[i] == self.content_to_check[i]
+            # Deep diff
+            if not result:
+                logger.info("Not matched")
+                diff = deepdiff.DeepDiff(self.content_expected[i], self.content_to_check[i])
+                logger.info(json.dumps(diff, indent=4))
+            return result
+
+    def highlight_expected_with_student_results(self):
+        pass
+
+
+class ScriptSimpleOutput(object):
+    # TODO Script basic Abstract class
+
+    def __init__(self, command, exec_dir, testing_arg, result_expected):
+        # TODO work with args set -> results set
         self.command = command
+        self.exec_dir = exec_dir
         self.testing_arg = testing_arg
         self.command_with_arg = f'{command} {testing_arg}'
         self.result_expected = result_expected
-        self.result = ""
+        self.output_full = None
+        self.output_strip = None
         self.passed = False
         # TODO score counter
         self.score = 0
 
     def check_full_match(self):
         logger.debug(f'Expected result: {self.result_expected}')
-        logger.debug(f'Student  result: {self.result}')
-        self.passed = self.result_expected == self.result
+        logger.debug(f'Student  result: {self.output_strip}')
+        self.passed = self.result_expected == self.output_strip
         logger.debug(f'Passed: {self.passed}')
         return self.passed
 
     def check_substring_like(self):
         logger.debug(f'Expected result: {self.result_expected}')
-        logger.debug(f'Student  result: {self.result}')
-        self.passed = self.result_expected.upper() in self.result.upper()
+        logger.debug(f'Student  result: {self.output_strip}')
+        self.passed = self.result_expected.upper() in self.output_strip.upper()
         logger.debug(f'Passed: {self.passed}')
         return self.passed
 
     def highlight_expected_with_student_results(self):
         return f"""==============Completed==============
+                     To check: {self.command_with_arg}
                      Expected: {self.result_expected}
-                     Result: {self.result}
-                     Passed: {self.passed} 
+                     Result  : {self.output_strip}
+                     Passed  : {self.passed} 
         ============================================"""
 
 
 if __name__ == '__main__':
-    executor = Executor()
+    DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 
     # TODO read params from config files
     COMMAND = "./netcalc.sh"
-
-    FROM_DIR = f'{os.path.dirname(os.path.realpath(__file__))}/scripts/'
+    EXEC_DIR = os.path.join(DIR_PATH, "scripts")
     ARG = "--host 192.168.100.255/24"
     EXPECTED_RESULT = "Not Valid"
 
     # TODO - check script file existence before check
-    script1 = Script(COMMAND, ARG, EXPECTED_RESULT)
-    logger.info(f'Checking {script1.command_with_arg} for getting [{EXPECTED_RESULT}]')
-    if not executor.execute(script1.command_with_arg, FROM_DIR):
+    script1 = ScriptSimpleOutput(COMMAND, EXEC_DIR, ARG, EXPECTED_RESULT)
+    logger.info(f'Checking {script1.command_with_arg} \n\t\tExptected: {EXPECTED_RESULT}')
+
+    executor = ScriptExecutor()
+    if not executor.execute(script1):
         # TODO check exit_code errors and sort out issues: stud script failure OR this script failure
         logger.warning("Execute failed")
         exit(0)
-    script1.result = executor.stripped_output
 
     logger.info(f'Passed: {script1.check_substring_like()}')
     logger.info(script1.highlight_expected_with_student_results())
+
+    logger.info("Checking csv results:")
+    csv1 = os.path.join(DIR_PATH, "scripts/accounts.csv")
+    csv2 = os.path.join(DIR_PATH, "scripts/accounts_new.csv")
+    csv_checker = ResultCheckerCSV(csv1, csv2)
+    csv_checker.compare_dictionaries()
